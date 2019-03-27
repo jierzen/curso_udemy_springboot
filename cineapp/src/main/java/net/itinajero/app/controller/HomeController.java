f11c0d6a44db8eb757c1c4dee8335d6e4810f5f9
jorge <jorge.esp.ram@gmail.com>
2019-03-26 20:51:54 -0400

    clase 37

--- cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.itinajero.app.model.Pelicula;
 
@@ -36,8 +37,10 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping(value="/detail/{id}/{fecha}", method=RequestMethod.GET)
-	public String mostrarDetalle(Model model,@PathVariable("id") int idPelicula,@PathVariable("fecha") String fecha) {
+	//@RequestMapping(value="/detail/{id}/{fecha}", method=RequestMethod.GET)
+	@RequestMapping(value="/detail", method=RequestMethod.GET)
+	//public String mostrarDetalle(Model model,@PathVariable("id") int idPelicula,@PathVariable("fecha") String fecha) {
+	public String mostrarDetalle(Model model,@RequestParam("idMovie") int idPelicula,@RequestParam("fecha") String fecha) {
 		
 		System.out.println("Buscando horarios para la pelicula " + idPelicula);
 		System.out.println("Para la fecha " + fecha);
